d285d95a68602aad44306fc4015e12289b1c5c48
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2022-07-25 19:20:52 -0300

    starting login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,16 @@
-import './styles.scss'
+import "./styles.scss";
 
 export default function Login() {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    );
+  return (
+    <div className="doctypehtml">
+      <header></header>
+      <main>
+            <div className="login-container">
+                <div className="loginBar">
+                    <h1>Login</h1>
+                </div>
+            </div>
+      </main>
+    </div>
+  );
 }
