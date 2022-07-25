6398ca53867dd69fa558e2d6c8fb7e668cc49c72
LeonardoRafaelli <leonardorafaelli@gmail.com>
2022-07-25 18:53:08 -0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,9 @@ import './index.css'
 import Header from './components/header/Header';
 import TeacherHome from './pages/teacher/home';
 
-<<<<<<< HEAD
 import AttendantHome from './pages/attendant/home';
 
 import Login from './pages/login';
-=======
-import Login from './pages/login/index';
->>>>>>> b85a08cfa62dc3cc8e3002158e0afc09493f5c47
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -22,12 +18,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <div className="screen-content">
     <Router>
       <Routes>
-<<<<<<< HEAD
         <Route path="/login" element={<Login/>} />
         <Route path='/attendant' element={<AttendantHome />} />
-=======
-        <Route path="/login" element={<Login />} />
->>>>>>> b85a08cfa62dc3cc8e3002158e0afc09493f5c47
         <Route path='/teacher'>
           <Route path='home' element={<TeacherHome />} />
         </Route>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-import React from 'react';
->>>>>>> b85a08cfa62dc3cc8e3002158e0afc09493f5c47
 import './styles.scss'
 
 export default function Login() {
