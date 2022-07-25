cf74d860a658d2284b819e70d893ac0ee3fa1f30
LeonardoRafaelli <leonardorafaelli@gmail.com>
2022-07-25 18:50:26 -0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,27 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 import './index.css'
 
+import Header from './components/header/Header';
 import TeacherHome from './pages/teacher/home';
+
+import AttendantHome from './pages/attendant/home';
+
+import Login from './pages/login';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Header/>
+
+    <div className="screen-content">
     <Router>
       <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path='/attendant' element={<AttendantHome />} />
         <Route path='/teacher'>
-          <Route path='/home' element={<TeacherHome />} />
+          <Route path='home' element={<TeacherHome />} />
         </Route>
       </Routes>
     </Router>
+    </div>
   </React.StrictMode>
 )
