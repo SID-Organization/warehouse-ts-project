b85a08cfa62dc3cc8e3002158e0afc09493f5c47
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2022-07-25 18:50:22 -0300

    login

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,13 @@ import './index.css'
 
 import TeacherHome from './pages/teacher/home';
 
+import Login from './pages/login/index';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path='/teacher'>
           <Route path='/home' element={<TeacherHome />} />
         </Route>
