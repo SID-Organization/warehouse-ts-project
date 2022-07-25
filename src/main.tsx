b7852f2603f5eb59a3dd33f0addd50a61be2f955
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2022-07-25 18:47:08 -0300

    commit branch

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import './index.css'
+import "./index.css";
 
-import TeacherHome from './pages/teacher/home';
+import TeacherHome from "./pages/teacher/home";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import Login from "./pages/login";
+import Register from "./pages/register";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path='/teacher'>
-          <Route path='/home' element={<TeacherHome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/teacher">
+          <Route path="/home" element={<TeacherHome />} />
         </Route>
       </Routes>
     </Router>
   </React.StrictMode>
-)
+);
