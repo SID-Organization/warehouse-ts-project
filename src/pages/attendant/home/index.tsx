b3cde1bc765bf7323c01119c8d67fbe1fe75aaa0
leonardorafaelli <leonardorafaelli@gmail.com>
2022-07-26 15:26:26 -0300

    updating on master, sorry

--- src/pages/attendant/home/index.tsx
+++ src/pages/attendant/home/index.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react';
 import './styles.scss'
 
 
 export default function AttendantHome() {
+
+    const [today, setToday] = useState(new Date().toLocaleDateString());
+
+    useEffect(() => {
+        console.log(today);
+    },[])
+
     return (
         <div className=" attendant-home">
             <div className='page-title'>
                 <h1>Reservas</h1>
+            </div>
+            <div className="todays-date">
+                <p>
+                    {today}
+                </p>
             </div>
         </div>
     );
