96f4b02c81f29160bac86b1fc33a9e0216fd79b6
leonardorafaelli <leonardorafaelli@gmail.com>
2022-07-27 18:06:30 -0300

    update

---
+++ src/components/reserve-card/ReserveCard.tsx
@@ -0,0 +1,9 @@
+import './styles.scss';
+
+import React from 'react'
+
+export default function ReserveCard() {
+  return (
+    <div>ReserveCard</div>
+  )
+}
--- src/pages/attendant/home/index.tsx
+++ src/pages/attendant/home/index.tsx
@@ -1,24 +1,38 @@
 import { useEffect, useState } from 'react';
 import './styles.scss'
 
+import ReserveCard from '../../../components/reserve-card/ReserveCard';
 
 export default function AttendantHome() {
 
     const [today, setToday] = useState(new Date().toLocaleDateString());
 
     useEffect(() => {
-        console.log(today);
-    },[])
+    }, [])
 
     return (
         <div className=" attendant-home">
             <div className='page-title'>
                 <h1>Reservas</h1>
             </div>
-            <div className="todays-date">
-                <p>
-                    {today}
-                </p>
+            <div className="info-container">
+                <div className="todays-date">
+                    <p>
+                        Hoje, {today}
+                    </p>
+                </div>
+                <button className="button-filter">
+                    <div className="content">
+                        <p>Filtros</p>
+                    </div>
+                    <div className="sort-icon">
+                    </div>
+                </button>
+            </div>
+            <div className="reserve-cards">
+                <ReserveCard />
+                <ReserveCard />
+                <ReserveCard />
             </div>
         </div>
     );
