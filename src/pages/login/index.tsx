48a2465c272d337f37490ed5f763e2589eec3fd8
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2022-07-28 10:09:21 -0300

    login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,99 @@
-import './styles.scss'
+import "./styles.scss";
+
+import { Link } from "react-router-dom";
+
+import { useEffect, useState } from "react";
+
+import { Icon } from "react-icons-kit";
+import { eyeOff } from "react-icons-kit/feather/eyeOff";
+import { eye } from "react-icons-kit/feather/eye";
 
 export default function Login() {
-    return (
-        <div>
-            <h1>Login</h1>
+  const [type, setType] = useState("password");
+  const [icon, setIcon] = useState(eyeOff);
+
+  useEffect(() => {}, [icon]);
+
+  const handleToggle = () => {
+    if (type === "password") {
+      setType("text");
+      setIcon(eyeOff);
+    } else {
+      setType("password");
+      setIcon(eye);
+    }
+  };
+  return (
+    <div className="loginPage">
+      <header>
+        <div className="containerHeader">
+          <img
+            className="logoSenai"
+            src="  https://estudante.sesisenai.org.br/img/login/sesi-senai-branca.webp"
+          />
         </div>
-    );
+      </header>
+      <div className="image"></div>
+      <main>
+        <div className="containerLogin">
+          <div className="containerBar">
+            <div className="welcomeTitle">
+              <h1 className="welcomeText">Bem-vindo</h1>
+            </div>
+          </div>
+          <div className="containerForms">
+            <div className="containerCredentials">
+              <div className="containerUser">
+                <img className="loginIcon" src="../../assets/user.png" />
+                <input
+                  className="inputUser"
+                  type="text"
+                  placeholder="Usuário"
+                />
+              </div>
+              <div className="containerPassword">
+                <img className="loginIcon" src="../../assets/passwd-lock.png" />
+                <input id="inputPassword" type={type} placeholder="Senha" />
+                <span onClick={handleToggle}>
+                  <Icon icon={icon} size="22" />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="passwordForgotten">
+            <a className="forgotPasswordText" href="#">
+              Esqueci minha senha
+            </a>
+          </div>
+          <div className="containerButtons">
+            <div className="containerLoginButton">
+              <Link to="/teacher/home">
+                <button className="buttonLogin">Entrar</button>
+              </Link>
+            </div>
+            <div className="containerAnotherOptionToLogin">
+              <div className="anotherOptionText">
+                <p>Ou</p>
+              </div>
+            </div>
+            <div className="containerGoogleLoginButton">
+              <div className="containerGoogleLogo">
+                <img
+                  className="googleLoginIcon"
+                  src="../../assets/google-logo.png"
+                />
+              </div>
+
+              <button className="buttonGoogleLogin">Entrar com Google</button>
+            </div>
+          </div>
+          <div className="containerRegister">
+            <Link className="registerText" to="/register">
+              Não tem uma conta? Registre-se
+            </Link>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
