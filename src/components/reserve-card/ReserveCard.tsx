1f462deb88ff78cafc06331d0ce57863827ca540
leonardorafaelli <leonardorafaelli@gmail.com>
2022-07-27 22:00:01 -0300

    update

--- src/components/reserve-card/ReserveCard.tsx
+++ src/components/reserve-card/ReserveCard.tsx
@@ -1,9 +1,28 @@
 import './styles.scss';
 
-import React from 'react'
+import avatar from '../../assets/avatar-icon.png';
 
 export default function ReserveCard() {
   return (
-    <div>ReserveCard</div>
+    <>
+        <div className='reserve-card'>
+            <div className="user-profile">
+                <img src={avatar} alt="" />
+                <div className="user-infos">
+                    <div className="user-name">
+                        
+                    </div>
+                    <div className="user-area">
+
+                    </div>
+                </div>
+            </div>
+            <div className="card-infos">
+                ReserveCard
+
+            </div>
+        </div>
+    
+    </>
   )
 }
