185ab02375d86c1e716c62d841c49b4fb42d183c
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2022-07-28 10:15:19 -0300

    register route

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import PendentReserves from './pages/attendant/pendent-reserves';
 import AttendantSideBar from './pages/attendant/side-bar';
 
 import Login from './pages/login';
+import Register from './pages/register';
 
 export default function App() {
     return (
@@ -20,6 +21,7 @@ export default function App() {
                 <Router>
                     <Routes>
                         <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
 
                         <Route path='atendente'>
                             <Route path='' element={<Header />}>
