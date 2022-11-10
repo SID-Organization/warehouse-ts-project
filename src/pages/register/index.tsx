6d35ccd300c77faee0bca24f1bca4a2f16cb037d
leonardorafaelli <leonardorafaelli@gmail.com>
2022-11-09 21:55:03 -0300

    feat: register a new user on system

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./styles.scss";
 
 import { Link } from "react-router-dom";
@@ -8,18 +8,42 @@ import { eyeOff } from "react-icons-kit/feather/eyeOff";
 import { eye } from "react-icons-kit/feather/eye";
 import toast, { Toaster } from "react-hot-toast";
 
-import email from "../../assets/email.png";
+import emailIcon from "../../assets/email.png";
 import goBack from "../../assets/goback.png";
-import user from "../../assets/user.png";
-import passLock from "../../assets/passwd-lock.png";
-import confirmPass from "../../assets/confirm-passwd.png";
+import userIcon from "../../assets/user.png";
+import passLockIcon from "../../assets/passwd-lock.png";
+import confirmPassIcon from "../../assets/confirm-passwd.png";
+import matriculaIcon from "../../assets/carteirinha-matricula.png";
 
 export default function Register() {
   const [type, setType] = useState("password");
   const [icon, setIcon] = useState(eyeOff);
 
-  function checkButtonClicked() {
+  const [matricula, setMatricula] = useState<number>();
+  const [usuario, setUsuario] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmaSenha, setConfirmaSenha] = useState("");
+  const [wrongPassFeedback, setWrongPassFeedback] = useState(false);
 
+  function getPostData() {
+    const [nome, ...sobrenomes] = usuario.split(" ");
+    const sobrenome = sobrenomes.join(" ");
+
+    const dataToSend = {
+      matricula: matricula,
+      nome: nome,
+      sobrenome: sobrenome,
+      email: email,
+      senha: senha,
+      cargo: "PENDENTE"
+    };
+
+    console.info(dataToSend);
+    return dataToSend;
+  }
+
+  function showSuccessToast() {
     toast.success("Registro solicitado com sucesso!", {
       style: {
         background: "#0047B5",
@@ -31,8 +55,56 @@ export default function Register() {
       },
       duration: 3500
     });
-    
   }
+
+  function showErrorToast() {
+    toast.error("Erro ao solicitar registro!", {
+      style: {
+        background: "#FF0000",
+        color: "#fff"
+      },
+      iconTheme: {
+        primary: "#fff",
+        secondary: "#FF0000"
+      },
+      duration: 3500
+    });
+  }
+
+  async function sendDataToDB() {
+    if (senha === confirmaSenha && senha !== "") {
+      await fetch("http://localhost:8080/almoxarifado/pessoas", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(getPostData())
+      }).then(response => {
+        // if (response.status === 200) {
+        //   showSuccessToast();
+        // } else {
+        //   showErrorToast();
+        // }
+        return response.json();
+      })
+      .then(data => console.log(data));
+    } else {
+      setWrongPassFeedback(true);
+    }
+  }
+
+  useEffect(
+    () => {
+      if (wrongPassFeedback) {
+        const wrpass = setTimeout(() => {
+          setWrongPassFeedback(false);
+        }, 3000);
+
+        return () => clearTimeout(wrpass);
+      }
+    },
+    [wrongPassFeedback]
+  );
 
   const handleToggle = () => {
     if (type === "password") {
@@ -71,48 +143,94 @@ export default function Register() {
             </div>
             <div className="containerGoBack" />
           </div>
-          <div className="containerForms">
-            <div className="containerCredentials">
-              <div className="containerEmail">
-                <img className="loginIcon" src={email} alt="" />
-                <input
-                  className="inputEmail"
-                  type="text"
-                  placeholder="E-mail"
-                />
-              </div>
-              <div className="containerUser">
-                <img className="loginIcon" src={user} alt="" />
-                <input
-                  className="inputUser"
-                  type="text"
-                  placeholder="Usuário"
-                />
-              </div>
-              <div className="containerPassword">
-                <img className="loginIcon" src={passLock} alt="loginIcon" />
-                <input id="inputPassword" type={type} placeholder="Senha" />
-                <span onClick={handleToggle}>
-                  <Icon icon={icon} size={25} />
-                </span>
-              </div>
-              <div className="containerConfirmPassword">
-                <img className="loginIcon" src={confirmPass} alt="loginIcon" />
-                <input
-                  id="inputPassword"
-                  type="password"
-                  placeholder="Confirmar senha"
-                />
+
+          <form action="#">
+            <div className="containerForms">
+              <div className="containerCredentials">
+                <div className="containerEmail">
+                  <img className="loginIcon" src={emailIcon} alt="" />
+                  <input
+                    className="inputEmail"
+                    type="text"
+                    placeholder="E-mail"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="containerEmail">
+                  <img className="loginIcon" src={matriculaIcon} alt="" />
+                  <input
+                    className="inputEmail"
+                    type="text"
+                    placeholder="Matrícula"
+                    value={matricula}
+                    onChange={e => setMatricula(parseInt(e.target.value))}
+                  />
+                </div>
+                <div className="containerUser">
+                  <img className="loginIcon" src={userIcon} alt="" />
+                  <input
+                    className="inputUser"
+                    type="text"
+                    placeholder="Nome completo"
+                    value={usuario}
+                    onChange={e => setUsuario(e.target.value)}
+                  />
+                </div>
+                <div className="containerPassword">
+                  <img
+                    className="loginIcon"
+                    src={passLockIcon}
+                    alt="loginIcon"
+                  />
+                  <input
+                    id="inputPassword"
+                    type={type}
+                    placeholder="Senha"
+                    value={senha}
+                    onChange={e => setSenha(e.target.value)}
+                  />
+                  <span onClick={handleToggle}>
+                    <Icon icon={icon} size={25} />
+                  </span>
+                </div>
+                <div className="containerConfirmPassword">
+                  <img
+                    className="loginIcon"
+                    src={confirmPassIcon}
+                    alt="loginIcon"
+                  />
+                  <input
+                    id="inputPassword"
+                    type="password"
+                    placeholder="Confirmar senha"
+                    value={confirmaSenha}
+                    onChange={e => setConfirmaSenha(e.target.value)}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="containerButtonsRegister">
-            <div className="containerLoginButton">
-              <button onClick={checkButtonClicked} className="buttonRegister">
-                Solicitar registro
-              </button>
+            <div style={{ height: "5px" }}>
+              {wrongPassFeedback &&
+                <div className="containerWrongPassFeedback">
+                  <p className="wrongPassFeedback">
+                    Senhas não conferem, por favor, tente novamente.
+                  </p>
+                </div>}
             </div>
-          </div>
+
+            <div className="containerButtonsRegister">
+              <div className="containerLoginButton">
+                <button
+                  type="submit"
+                  onClick={sendDataToDB}
+                  className="buttonRegister"
+                >
+                  Solicitar registro
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
       </main>
     </div>
